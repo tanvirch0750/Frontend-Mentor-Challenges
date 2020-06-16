8caffd31b5ad66c82e76fbf0ad20f4fc6697f646
tanvirch0750 <tanvir0750@yahoo.com>
2020-06-16 19:49:36 +0600

    customize base file

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ const projects = [
   "base-apparel-coming-soon-master",
   "intro-component-with-signup-form-master",
   "single-price-grid-component-master"
-]
+];
 
-projects.forEach(project => {
+const list = document.getElementById('list');
+
+projects.forEach((project, i) => {
+  const listItem = document.createElement('li');
   const link = document.createElement('a');
-  link.src = project;
-})
+  link.href = `/${project}/index.html`;
+  link.innerText = `${i + 1}. ${formatProjectName(project)}`;
+
+  const img = document.createElement('img');
+  img.src = `/${project}/design/desktop-design.jpg`;
+
+  list.appendChild(listItem);
+  link.prepend(img);
+  listItem.appendChild(link);
+
+});
+
+function formatProjectName(name) {
+  return name
+    .split('-')
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+}
